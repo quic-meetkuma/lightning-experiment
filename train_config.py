047d663bd8e208a311c3171c6aa445db85f8a310
Meet Patel <meetkuma@qti.qualcomm.com>
2025-09-01 11:10:25 +0530

    Added train config
    Signed-off-by: meetkuma <meetkuma@qti.qualcomm.com>

---
+++ train_config.py
@@ -0,0 +1,15 @@
+# ==============================================================================
+#
+#  Copyright (c) Qualcomm Technologies, Inc.
+#  All Rights Reserved.
+#  Confidential and Proprietary - Qualcomm Technologies, Inc.
+#
+# ==============================================================================
+
+num_epochs = 10
+warmup_epochs = 2
+batch_size = 1
+max_seq_len = 512
+num_workers = 4
+use_peft = True
+train_data_path = "train_data"
